404fb676f6630bf0c2ecbf43a2c0481b832aaae0
Muhammad Usman <osmanamin689@gmail.com>
2022-08-12 22:35:27 +0500

    Errors

--- Forum_User/urls.py
+++ Forum_User/urls.py
@@ -8,16 +8,16 @@ from Forum_User import views
 from Social_Forum import settings
 
 urlpatterns = [
-<<<<<<< HEAD
+
     path('<str:username>/', views.account_overview, name="account_overview"),
     path('<str:username>/details/', views.account_details, name="account_details"),
     path('<str:username>/settings/', views.account_settings, name="account_settings"),
-=======
+
     path('', views.account_overview, name="account_overview"),
     path('details/', views.account_details, name="account_details"),
     path('settings/', views.account_settings, name="account_settings"),
     path('edit-profile/', views.edit_profile, name="edit_profile"),
->>>>>>> 1e546d8ec17e137ccd60bed15d83fd8891931e74
+
     path('upload-post/', views.upload_post, name="upload_post"),
     path('add-comment/', views.add_comment, name="add_comment"),
     path('add-vote/', views.add_vote, name="add_vote"),
